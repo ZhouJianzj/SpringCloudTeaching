de00fce51312c3f3c80c361b4b57a1c8d0270c35
ZhouJianzj <2998390669@qq.com>
2023-10-12 15:56:25 +0800

    Stream 消息组以及消息分区的实现，Sleuth 整合 Zipkin 实现链路追踪数据收集以及可视化，实现了使用Sleuth和rabbitMQ收集链路数据，实现了通过Sleuth和Zipkin将收集到的链路数据存储到Mysql之中

--- demo01-Zipkin-Zuul/src/main/java/com/example/demo01zipkinzuul/filter/MyZipkinFilter.java
+++ demo01-Zipkin-Zuul/src/main/java/com/example/demo01zipkinzuul/filter/MyZipkinFilter.java
@@ -3,10 +3,11 @@ package com.example.demo01zipkinzuul.filter;
 import brave.Tracer;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Component
@@ -28,9 +29,15 @@ public class MyZipkinFilter extends ZuulFilter {
 
     @Autowired
     Tracer tracer;
+    @Autowired
+    HttpServletRequest request;
     @Override
     public Object run() throws ZuulException {
+
         tracer.currentSpan().tag("name","zhoujian");
+        String remoteAddr = request.getRemoteAddr();
+        tracer.currentSpan().tag("Remote Address",remoteAddr);
+
         return null;
     }
 }
